d925810a149ee8983ef4fb39430c568f167f3bcd
Grace Chiang <gracechiang@me.com>
2018-03-09 00:26:35 +0000

    cleaned up the code, delete cascades in a separate file wipe_everything

--- db/seeds/04_users.js
+++ db/seeds/04_users.js
@@ -1,11 +1,8 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice', email: 'Alice@Alice.com',type_id:1,password:'123',phone_number:'7453487534'}),
-        knex('users').insert({id: 2, name: 'Brad', email: 'Brad@Brad.com',type_id:1,password:'123',phone_number:'7453487534'}),
-        knex('users').insert({id: 3, name: 'Grace', email: 'Grace@Grace.com',type_id:2,password:'123',phone_number:'7453487534'}),
-        knex('users').insert({id: 4, name: 'Di', email: 'Di@Di.com',type_id:3,password:'123',phone_number:'7453487534'})
-      ]);
-    });
+  return Promise.all([
+    knex('users').insert({id: 1, name: 'Alice', email: 'Alice@Alice.com',type_id:1,password:'123',phone_number:'7453487534'}),
+    knex('users').insert({id: 2, name: 'Brad', email: 'Brad@Brad.com',type_id:1,password:'123',phone_number:'7453487534'}),
+    knex('users').insert({id: 3, name: 'Grace', email: 'Grace@Grace.com',type_id:2,password:'123',phone_number:'7453487534'}),
+    knex('users').insert({id: 4, name: 'Di', email: 'Di@Di.com',type_id:3,password:'123',phone_number:'7453487534'})
+  ]);
 };
--- db/seeds/09_work_schedules.js
+++ db/seeds/09_work_schedules.js
@@ -1,11 +1,8 @@
 exports.seed = function (knex, Promise) {
-  return knex('work_schedules').del()
-    .then(function () {
-      return Promise.all([
-        knex('work_schedules').insert({ id: 1, user_staff_id: 1, shift_start: 'Jan 1, 2018 10:00', shift_end: 'Jan 1, 2018 19:00' }),
-        knex('work_schedules').insert({ id: 2, user_staff_id: 2, shift_start: 'March 28, 2018 10:00', shift_end: 'March 28, 2018 19:00' }),
-        knex('work_schedules').insert({ id: 4, user_staff_id: 4, shift_start: 'October 29, 2018 10:00', shift_end: 'October 29, 2018 19:00' }), 
-        knex('work_schedules').insert({ id: 3, user_staff_id: 3, shift_start: 'March 20, 2018 10:00', shift_end: 'March 28, 2018 19:00' })
-      ])
-    });
+  return Promise.all([
+    knex('work_schedules').insert({ id: 1, user_staff_id: 1, shift_start: 'Jan 1, 2018 10:00', shift_end: 'Jan 1, 2018 19:00' }),
+    knex('work_schedules').insert({ id: 2, user_staff_id: 2, shift_start: 'March 28, 2018 10:00', shift_end: 'March 28, 2018 19:00' }),
+    knex('work_schedules').insert({ id: 4, user_staff_id: 4, shift_start: 'October 29, 2018 10:00', shift_end: 'October 29, 2018 19:00' }), 
+    knex('work_schedules').insert({ id: 3, user_staff_id: 3, shift_start: 'March 20, 2018 10:00', shift_end: 'March 28, 2018 19:00' })
+  ])
 };
