213ef695b6d7600cf6e8e84e81e8a2ba070f3bd4
Di Wu <wd1991610@gmail.com>
2018-03-11 20:49:11 -0700

    fix seeding

--- db/seeds/04_users.js
+++ db/seeds/04_users.js
@@ -1,8 +1,8 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
-    knex('users').insert({id: 1, name: 'Alice', email: 'Alice@Alice.com',type_id:1,password:'123',phone_number:'7453487534'}),
-    knex('users').insert({id: 2, name: 'Brad', email: 'Brad@Brad.com',type_id:1,password:'123',phone_number:'7453487534'}),
-    knex('users').insert({id: 3, name: 'Grace', email: 'Grace@Grace.com',type_id:2,password:'123',phone_number:'7453487534'}),
-    knex('users').insert({id: 4, name: 'Di', email: 'Di@Di.com',type_id:3,password:'123',phone_number:'7453487534'})
+    knex('users').insert({name: 'Alice', email: 'alice@alice.com',type_id:1,password:'$2a$10$4bLzLwKjvSmdwdwhL/wZoeyjv1lAqEO8TISJXNVXtjFPM4Cz4jtcW',phone_number:'7453487534'}),
+    knex('users').insert({name: 'Brad', email: 'brad@brad.com',type_id:1,password:'$2a$10$4bLzLwKjvSmdwdwhL/wZoeyjv1lAqEO8TISJXNVXtjFPM4Cz4jtcW',phone_number:'7453487534'}),
+    knex('users').insert({name: 'Grace', email: 'grace@grace.com',type_id:2,password:'$2a$10$4bLzLwKjvSmdwdwhL/wZoeyjv1lAqEO8TISJXNVXtjFPM4Cz4jtcW',phone_number:'7453487534'}),
+    knex('users').insert({name: 'Di', email: 'di@di.com',type_id:3,password:'$2a$10$4bLzLwKjvSmdwdwhL/wZoeyjv1lAqEO8TISJXNVXtjFPM4Cz4jtcW',phone_number:'7453487534'})
   ]);
 };
